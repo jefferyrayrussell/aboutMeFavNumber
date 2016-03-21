1b657f9f6c2d76a31124156f05f383acced766db
vbenavente <vbenavente@hotmail.com>
2016-03-21 16:43:12 -0700

    functions added to app.js

--- app.js
+++ app.js
@@ -13,77 +13,92 @@ var totalQuestions = 0;
 /* ask the first yes or no question using a prompt*/
 var ques1 = document.getElementById('ans1');
 
-var firstQuestion = prompt('Was I born in Renton, Washington? Please answer yes or no.');
-totalQuestions ++;
-/* put user response in correct format, y to YES, yes to YES, and evaluate if the first question has been answered correctly,*/
-if(firstQuestion.toUpperCase() === 'Y' || firstQuestion.toUpperCase() === 'YES'){
-  console.log(firstQuestion);
-  ques1.textContent = 'Yay, you are correct!';
-  correctAnswers ++;
-} else {
-  ques1.textContent = 'Sorry, you are incorrect.';
+function q1 () {
+  var firstQuestion = prompt('Was I born in Renton, Washington? Please answer yes or no.');
+  totalQuestions ++;
+  /* put user response in correct format, y to YES, yes to YES, and evaluate if the first question has been answered correctly,*/
+  if(firstQuestion.toUpperCase() === 'Y' || firstQuestion.toUpperCase() === 'YES'){
+    console.log(firstQuestion);
+    ques1.textContent = 'Yay, you are correct!';
+    correctAnswers ++;
+  } else {
+    ques1.textContent = 'Sorry, you are incorrect.';
+  }
 }
+q1();
 
 var ques2 = document.getElementById('ans2');
 
-var secondQuestion = prompt('Do I have three college educated daughters? Please answer yes or no.');
-totalQuestions ++;
-if(secondQuestion.toUpperCase() === 'Y' || secondQuestion.toUpperCase() === 'YES'){
-  ques2.textContent = 'Yay, you are correct!';
-  correctAnswers ++;
-} else {
-  ques2.textContent = 'Sorry, you are incorrect.';
+function q2() {
+  var secondQuestion = prompt('Do I have three college educated daughters? Please answer yes or no.');
+  totalQuestions ++;
+  if(secondQuestion.toUpperCase() === 'Y' || secondQuestion.toUpperCase() === 'YES'){
+    ques2.textContent = 'Yay, you are correct!';
+    correctAnswers ++;
+  } else {
+    ques2.textContent = 'Sorry, you are incorrect.';
+  }
+  console.log(secondQuestion);
 }
-
-console.log(secondQuestion);
+q2();
 
 var ques3 = document.getElementById('ans3');
 
-var thirdQuestion = prompt('Do I enjoy wakeskating and karaoke? Please answer yes or no.');
-totalQuestions ++;
-if(thirdQuestion.toUpperCase() === 'Y' || thirdQuestion.toUpperCase() === 'YES'){
-  ques3.textContent = 'Yay, you are correct!';
-  correctAnswers ++;
-} else {
-  ques3.textContent = 'Sorry, you are incorrect.';
+function q3() {
+  var thirdQuestion = prompt('Do I enjoy wakeskating and karaoke? Please answer yes or no.');
+  totalQuestions ++;
+  if(thirdQuestion.toUpperCase() === 'Y' || thirdQuestion.toUpperCase() === 'YES'){
+    ques3.textContent = 'Yay, you are correct!';
+    correctAnswers ++;
+  } else {
+    ques3.textContent = 'Sorry, you are incorrect.';
+  }
+  console.log(thirdQuestion);
 }
-
-console.log(thirdQuestion);
+q3();
 
 var ques4 = document.getElementById('ans4');
 
-var fourthQuestion = prompt('Is my favorite book "Wise Man" by Flannery OConnor? Please answer yes or no.');
-totalQuestions ++;
-if(fourthQuestion.toUpperCase() === 'Y' || fourthQuestion.toUpperCase() === 'YES'){
-  ques4.textContent = 'Yay, you are correct!';
-  correctAnswers ++;
-} else {
-  ques4.textContent = 'Sorry, you are incorrect.';
+function q4() {
+  var fourthQuestion = prompt('Is my favorite book "Wise Man" by Flannery OConnor? Please answer yes or no.');
+  totalQuestions ++;
+  if(fourthQuestion.toUpperCase() === 'Y' || fourthQuestion.toUpperCase() === 'YES'){
+    ques4.textContent = 'Yay, you are correct!';
+    correctAnswers ++;
+  } else {
+    ques4.textContent = 'Sorry, you are incorrect.';
+  }
+  console.log(fourthQuestion);
 }
-
-console.log(fourthQuestion);
+q4();
 
 var ques5 = document.getElementById('ans5');
 
-var fifthQuestion = prompt('Did I once own a Suzuki GSXR 1000? Please answer yes or no.');
-totalQuestions ++;
-if(fifthQuestion.toUpperCase() === 'Y' || fifthQuestion.toUpperCase() === 'YES'){
-  ques5.textContent = 'Yay, you are correct!';
-  correctAnswers ++;
-  console.log(fifthQuestion);
-
-  var ques6 = document.getElementById('ans6');
-
-  var sixthQuestion = prompt('Because you got that right you get to answer one more question. Is my favorite pie, apple?');
+function q5() {
+  var fifthQuestion = prompt('Did I once own a Suzuki GSXR 1000? Please answer yes or no.');
   totalQuestions ++;
-  if(sixthQuestion.toUpperCase() === 'Y' || sixthQuestion.toUpperCase() === 'YES'){
-    ques6.textContent = 'Yay, you are correct! Thank you for taking the time to learn about me.';
+  if(fifthQuestion.toUpperCase() === 'Y' || fifthQuestion.toUpperCase() === 'YES'){
+    ques5.textContent = 'Yay, you are correct!';
     correctAnswers ++;
-  } else {
-    ques6.textContent = 'Sorry, you are incorrect. But, thank you for taking the time to learn about me.';
+    console.log(fifthQuestion);
+
+    var ques6 = document.getElementById('ans6');
+
+    function q6() {
+      var sixthQuestion = prompt('Because you got that right you get to answer one more question. Is my favorite pie, apple?');
+      totalQuestions ++;
+      if(sixthQuestion.toUpperCase() === 'Y' || sixthQuestion.toUpperCase() === 'YES'){
+        ques6.textContent = 'Yay, you are correct! Thank you for taking the time to learn about me.';
+        correctAnswers ++;
+      } else {
+        ques6.textContent = 'Sorry, you are incorrect. But, thank you for taking the time to learn about me.';
+      }
+      console.log(sixthQuestion);
+    }
   }
+  q6();
 }
-console.log(sixthQuestion);
+q5();
 
 /* This code asks the user to guess a number. The user gets four chances
 and receives feedback after each guess whether the answer is high or low and is told
@@ -94,35 +109,38 @@ alert('Lastly, I want you to guess the jersey number I wore when I played High S
 var numberOfGuesses = 0;
 var ques7 = document.getElementById('ans7');
 
-while (numberGuessed !== 52 && numberOfGuesses < 4){
+function q7() {
+  while (numberGuessed !== 52 && numberOfGuesses < 4){
 
-  var numberGuessed = parseInt(prompt('Guess my football jersey number?'));
-  totalQuestions ++;
-  console.log('User guessed' + numberGuessed);
-
-  if(Number.isNaN(numberGuessed) === true){
-    ques7.textContent = 'I wore a number on my Jersey. Guess again please';
-    numberOfGuesses++;
+    var numberGuessed = parseInt(prompt('Guess my football jersey number?'));
+    totalQuestions ++;
     console.log('User guessed' + numberGuessed);
 
-  } else if (numberGuessed > 52){
-    ques7.textContent = 'You guessed high.';
-    numberOfGuesses++;
-    console.log('User guessed' + numberGuessed);
+    if(Number.isNaN(numberGuessed) === true){
+      ques7.textContent = 'I wore a number on my Jersey. Guess again please';
+      numberOfGuesses++;
+      console.log('User guessed' + numberGuessed);
 
-  } else if (numberGuessed < 52){
-    ques7.textContent = 'You guessed low.';
-    numberOfGuesses++;
-    console.log('User guessed' + numberGuessed);
+    } else if (numberGuessed > 52){
+      ques7.textContent = 'You guessed high.';
+      numberOfGuesses++;
+      console.log('User guessed' + numberGuessed);
 
-  } else {
-    ques7.textContent = 'You guessed correctly, I wore number 52.';
-    console.log('User guessed' + numberGuessed);
-    correctAnswers ++;
+    } else if (numberGuessed < 52){
+      ques7.textContent = 'You guessed low.';
+      numberOfGuesses++;
+      console.log('User guessed' + numberGuessed);
+
+    } else {
+      ques7.textContent = 'You guessed correctly, I wore number 52.';
+      console.log('User guessed' + numberGuessed);
+      correctAnswers ++;
+    }
   }
-}
 
-if (numberOfGuesses === 4 && numberGuessed !== 52){
-  alert('Too many guesses.  The clock has ran out.');
+  if (numberOfGuesses === 4 && numberGuessed !== 52){
+    alert('Too many guesses.  The clock has ran out.');
+  }
+  alert('Thank you for playing ' + userName + '.  Out of ' + totalQuestions + ' questions, you answered ' + correctAnswers + ' correctly.');
 }
-alert('Thank you for playing ' + userName + '.  Out of ' + totalQuestions + ' questions, you answered' + correctAnswers + ' correctly.');
+q7();
